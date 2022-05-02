26ca62f0c02d87d67984c04d24879ff2eab1427e
bin024 <100820966+bin024@users.noreply.github.com>
2022-05-02 16:55:33 -0400

    Create HONGBIN_LIU_assignment2.js

---
+++ HW2/HONGBIN_LIU_assignment2.js
@@ -0,0 +1,83 @@
+const itemsObject = [
+    {quantity: 1, price: 200},
+    {quantity: 3, price: 350},
+    {quantity: 5, price: 400}
+]
+
+// Given the array, implement a function for generating a new array which doubles the quantity and price in each object.
+
+function Q1 (arr,num) {
+    var result = []
+    for (var i in arr){
+      var temp = {}
+      for (var j in arr[i]){
+        temp[j] = arr[i][j]*num
+      }
+      result.push(temp)
+    }
+    return result
+  }
+  
+  console.log(Q1(itemsObject,2))
+
+// Given the array, implement a function for generating a new array which contains item quantity > 2 and price > 300 only.
+
+function Q2 (arr,quantity,price) {
+    var result = []
+    for (var i in arr){
+      var temp = {}
+      for (var j in arr[i]){
+        temp[j] = arr[i][j]
+      }
+      if (temp.quantity > quantity && temp.price > price){
+        result.push(temp)
+      }
+    }
+    return result
+  }
+  
+  console.log(Q2(itemsObject,2,300))
+
+//   Given the array, implement a function to calculate the total value of the items.
+function Q3 (arr){
+    let totalQuantity = 0, totalPrice = 0
+    arr.forEach (arr => {
+      totalQuantity += arr.quantity;
+      totalPrice += arr.price;
+    });
+    return {quantity:totalQuantity,price:totalPrice}
+  }
+  
+  console.log(Q3(itemsObject))
+
+
+const string = ' Perhaps The Easiest-to-understand  Case For Reduce Is To Return The Sum Of All The Elements In An Array ';
+
+// Given the string, implement a function to remove all the non-alphabet characters and extra space in the string and convert the string to all lowercase.
+function Q4 (str) {
+    return str.split('-').join(' ').split(' ').filter(e => String(e).trim()).join(' ').toLowerCase()
+}
+
+console.log(Q4(string))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
